e8844f153bc0e22b341d6b980d045ed7aca5da3b
masoncm97 <masoncmathew@gmail.com>
2021-04-18 13:56:57 -0500

    AMF logo flashes with color now, updated homepage text

--- all-my-friends/src/app/home/home.component.ts
+++ all-my-friends/src/app/home/home.component.ts
@@ -5,11 +5,28 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
+
 export class HomeComponent implements OnInit {
 
   constructor() { }
 
+  index = 0;
+  numLogos = 7;
+  displayedLogo = '';
+  hiddenLogo = '';
+
   ngOnInit(): void {
+    setInterval(() => this.changeLogo(), 1500);
   }
 
+  changeLogo() {
+    let displayLogo = document.getElementsByClassName(String(this.index % this.numLogos))[0] as HTMLElement;
+    let hideLogo = document.getElementsByClassName(this.hiddenLogo)[0] as HTMLElement;
+    if(hideLogo) {
+      hideLogo.style.display = "none"
+    }
+    displayLogo.style.display = "block";
+    this.hiddenLogo = String(this.index % this.numLogos)
+    this.index++;
+  }
 }
